2a2b9902e6674211458610c46015a937668b14a0
Neil Mathias <neilmathias25@gmail.com>
2026-06-28 13:39:29 +0530

    Update Exam Chapters

--- frontend/src/data/chapters.js
+++ frontend/src/data/chapters.js
@@ -1,19 +1,53 @@
 const chapters = {
   History: [
-    "Ancient History",
-    "Medieval History",
-    "Modern History",
+    "Ancient Indian History",
+    "Medieval Indian History",
+    "Modern Indian History",
   ],
 
   Geography: [
     "Indian Geography",
-    "World Geography",
+    "World Geography & Astronomy",
+  ],
+
+  Polity: [
+    "Constitution Of India",
+    "Union & State Government",
+    "Local Governance",
+    "Constitutional & Non-Constitutional Bodies",
+    "Key Amendments & Articles"
+  ],
+
+  Economy: [
+    "National Income & Economic Growth",
+    "Money, Banking, & Monetary Policy",
+    "Public Finance & Fiscal Policy",
+    "Market Structure & Demand-Supply",
+    "Economic Planning & Poverty",
+    "Foreign Trade & International Organizations"
   ],
 
   Science: [
     "Physics",
     "Chemistry",
     "Biology",
+  ],
+
+  CurrentAffairs: [
+    "National & State Affairs",
+    "Appointments & Resignations",
+    "Sports & Tournaments",
+    "Awards & Recognitions",
+    "Defense, Science, & Technology",
+    "Economy & Indices",
+    "Books & Authors"
+  ],
+
+  ComputerAwareness: [
+    "Computer Basics & Hardware",
+    "Software & Operating Systems",
+    "Internet & E-communication",
+    "Networking & Cyber Security"
   ],
 };
 
